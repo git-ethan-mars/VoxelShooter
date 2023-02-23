cc0cd9d4c9d4bcc43026e3b0553f933b3a10bdd7
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2023-02-24 01:36:29 +0500

    Synchronized builder rotation

--- Assets/Scripts/GamePlay/CameraController.cs
+++ Assets/Scripts/GamePlay/CameraController.cs
@@ -1,23 +1,32 @@
 using System;
+using Mirror;
 using UnityEngine;
 
 namespace GamePlay
 {
-    public class CameraController : MonoBehaviour
+    public class CameraController : NetworkBehaviour
     {
-        [SerializeField] private Transform objectToFollow;
+        [SerializeField] private GameObject cameraObject;
         [SerializeField] private float sensitivityX;
         [SerializeField] private float sensitivityY;
+        private Transform _cameraTransform;
+        private Camera Camera { get; set; }
         private float XRotation { get; set; }
         private float YRotation { get; set; }
-        private void Start()
+
+        public override void OnStartAuthority()
         {
+            cameraObject.SetActive(true);
+            Camera = cameraObject.GetComponent<Camera>();
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
+            _cameraTransform = Camera.transform;
+
         }
 
         private void Update()
         {
+            if (!isOwned) return;
             var mouseXInput = Input.GetAxisRaw("Mouse X");
             var mouseYInput = Input.GetAxisRaw("Mouse Y");
             var mouseX = mouseXInput * Time.deltaTime * sensitivityX;
@@ -25,9 +34,9 @@ namespace GamePlay
             YRotation += mouseX;
             XRotation -= mouseY;
             XRotation = Math.Clamp(XRotation, -90, 90);
+            _cameraTransform.rotation = Quaternion.Euler(XRotation, YRotation, 0);
+            _cameraTransform.position = transform.position; 
             transform.rotation = Quaternion.Euler(XRotation, YRotation, 0);
-            transform.position = objectToFollow.position;
-            objectToFollow.rotation = Quaternion.Euler(XRotation, YRotation, 0);
         }
     }
 }
