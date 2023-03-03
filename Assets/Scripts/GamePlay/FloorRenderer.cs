359285b3c8aa3c394e283956340870287a647fe8
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2023-03-03 20:23:55 +0500

    Update FloorRenderer.cs

--- Assets/Scripts/GamePlay/FloorRenderer.cs
+++ Assets/Scripts/GamePlay/FloorRenderer.cs
@@ -7,6 +7,7 @@ namespace GamePlay
     public class FloorRenderer : MonoBehaviour
     {
         public Map Map { get; set; }
+
         private void Start()
         {
             RegenerateMesh();
@@ -17,23 +18,16 @@ namespace GamePlay
             var mesh = new Mesh();
             var vertices = new List<Vector3>();
             var triangles = new List<int>();
-            for (var x = 0; x < Map.Width; x++)
-            {
-                for (var z = 0; z < Map.Depth; z++)
-                {
-                    vertices.Add( new Vector3Int(x, 0, z));
-                    vertices.Add(new Vector3Int(x, 0, z+1));
-                    vertices.Add(new Vector3Int(x+1, 0, z));
-                    vertices.Add(new Vector3Int(x+1, 0, z+1));
-                    triangles.Add(vertices.Count - 4);
-                    triangles.Add(vertices.Count - 3);
-                    triangles.Add(vertices.Count - 2);
-                    triangles.Add(vertices.Count - 3);
-                    triangles.Add(vertices.Count - 1);
-                    triangles.Add(vertices.Count - 2);
-                }
-            }
-
+            vertices.Add(new Vector3Int(0, 0, 0));
+            vertices.Add(new Vector3Int(0, 0, Map.Depth - 1));
+            vertices.Add(new Vector3Int(Map.Width - 1, 0, 0));
+            vertices.Add(new Vector3Int(Map.Width - 1, 0, Map.Depth - 1));
+            triangles.Add(0);
+            triangles.Add(1);
+            triangles.Add(2);
+            triangles.Add(1);
+            triangles.Add(3);
+            triangles.Add(2);
             mesh.indexFormat = IndexFormat.UInt32;
             mesh.vertices = vertices.ToArray();
             mesh.triangles = triangles.ToArray();
