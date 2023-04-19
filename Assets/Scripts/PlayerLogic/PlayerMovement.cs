b7a2954d6ed6bd744f80295818bd83e8d91b4b55
a.burgart02 <a.burgart02@gmail.com>
2023-04-20 02:34:46 +0500

    Fix jump

--- Assets/Scripts/PlayerLogic/PlayerMovement.cs
+++ Assets/Scripts/PlayerLogic/PlayerMovement.cs
@@ -1,33 +1,28 @@
-using System;
 using Data;
 using Infrastructure.Services;
 using Infrastructure.Services.Input;
 using Mirror;
-using Rendering;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace PlayerLogic
 {
-    [RequireComponent(typeof(Rigidbody))]
     public class PlayerMovement : NetworkBehaviour
     {
         [SyncVar] private float _speed;
         [SyncVar] private float _jumpMultiplier;
-        private Rigidbody Rigidbody { get; set; }
-        private bool _isGrounded;
-        private bool _isFell;
-        private bool _isJumpButtonPressed;
-        private float _previousFallingSpeed;
+        private CharacterController _characterController;
         private IInputService _inputService;
         private Vector3 _movementDirection;
-
-
+        private float _jumpSpeed;
+        private const float Gravity = -30f;
+        
         public void Construct(PlayerCharacteristic characteristic)
         {
             _speed = characteristic.speed;
             _jumpMultiplier = characteristic.jumpMultiplier;
         }
-
+        
         private void Awake()
         {
             _inputService = AllServices.Container.Single<IInputService>();
@@ -35,64 +30,27 @@ namespace PlayerLogic
 
         public override void OnStartLocalPlayer()
         {
-            Rigidbody = GetComponent<Rigidbody>();
-            Rigidbody.freezeRotation = true;
+            _characterController = GetComponent<CharacterController>();
         }
 
         private void Update()
         {
             if (!isLocalPlayer) return;
-            ReadInput();
-        }
-
-        private void ReadInput()
-        {
             var axis = _inputService.Axis.normalized;
             var playerTransform = transform;
             _movementDirection = (axis.x * playerTransform.forward + axis.y * playerTransform.right).normalized;
-            if (_inputService.IsJumpButtonDown())
+            if (_characterController.isGrounded)
             {
-                _isJumpButtonPressed = true;
-            }
-        }
-
-        private void FixedUpdate()
-        {
-            if (!isLocalPlayer) return;
-            if (IsPlayerInAir)
-            {
-                _isFell = true;
-                if (_isJumpButtonPressed && _isGrounded)
+                _jumpSpeed = 0;
+                if (_inputService.IsJumpButtonDown())
                 {
-                    Rigidbody.AddForce(Vector3.up * _jumpMultiplier, ForceMode.VelocityChange);
+                    _jumpSpeed = _jumpMultiplier;
                 }
             }
-            else
-            {
-                _isFell = false;
-            }
-
-            _isJumpButtonPressed = false;
-            _previousFallingSpeed = Rigidbody.velocity.y;
-            Rigidbody.velocity = _movementDirection * _speed + new Vector3(0, Rigidbody.velocity.y, 0);
-        }
-
-        private bool IsPlayerInAir => Math.Abs(_previousFallingSpeed - Rigidbody.velocity.y) < Constants.Epsilon;
-
-        private void OnCollisionStay(Collision collision)
-        {
-            if (collision.collider.gameObject.GetComponent<ChunkRenderer>() && _isFell)
-            {
-                _isGrounded = true;
-            }
-        }
-
-        private void OnCollisionExit(Collision collision)
-        {
-            if (collision.collider.gameObject.GetComponent<ChunkRenderer>())
-            {
-                _isGrounded = false;
-            }
+            _jumpSpeed += Gravity * Time.deltaTime;
+            Vector3 direction = new Vector3(_movementDirection.x * _speed * Time.deltaTime, _jumpSpeed * Time.deltaTime,
+                _movementDirection.z * Time.deltaTime * _speed);
+            _characterController.Move(direction);
         }
     }
 }
