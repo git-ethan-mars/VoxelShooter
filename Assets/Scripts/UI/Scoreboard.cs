80fc81b3ee520f3040681fde572f4a520ae3dd14
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2023-05-25 19:25:18 +0500

    Avatar loading fix

--- Assets/Scripts/UI/Scoreboard.cs
+++ Assets/Scripts/UI/Scoreboard.cs
@@ -3,6 +3,7 @@ using Data;
 using Infrastructure.Services.Input;
 using Infrastructure.Services.PlayerDataLoader;
 using Networking;
+using Steamworks;
 using UnityEngine;
 
 namespace UI
@@ -10,10 +11,12 @@ namespace UI
     [RequireComponent(typeof(CanvasGroup))]
     public class Scoreboard : MonoBehaviour
     {
-        [SerializeField] private List<Score> scores;
+        [SerializeField] private List<ScoreUI> scores;
+        private Dictionary<CSteamID, Texture2D> _avatarBySteamId;
         private IInputService _inputService;
         private CanvasGroup _canvasGroup;
         private IAvatarLoader _avatarLoader;
+        private List<ScoreData> _currentScoreboardData;
 
 
         public void Construct(IInputService inputService, IAvatarLoader playerDataLoader,
@@ -22,32 +25,43 @@ namespace UI
             networkManager.ScoreboardChanged += UpdateScoreboard;
             _inputService = inputService;
             _avatarLoader = playerDataLoader;
+            _avatarLoader.OnAvatarLoaded += ApplyAvatar;
             _canvasGroup = GetComponent<CanvasGroup>();
             _canvasGroup.alpha = 0;
+            _avatarBySteamId = new Dictionary<CSteamID, Texture2D>();
         }
 
         private void Update()
         {
             _canvasGroup.alpha = _inputService.IsScoreboardButtonHold() ? 1 : 0;
         }
-
-        private void UpdateScoreboard(List<ScoreData> scoreData)
+        private void UpdateScoreboard(List<ScoreData> scoreBoardData)
         {
+            _currentScoreboardData = scoreBoardData;
             for (var i = 0; i < scores.Count; i++)
             {
                 scores[i].gameObject.SetActive(false);
             }
 
-            for (var i = 0; i < scoreData.Count; i++)
+            for (var i = 0; i < scoreBoardData.Count; i++)
             {
-                var steamAvatar = _avatarLoader.LoadAvatar(scoreData[i].SteamID);
-                scores[i].avatar.texture = steamAvatar;
-                scores[i].nickName.SetText(scoreData[i].NickName);
-                scores[i].kills.SetText(scoreData[i].Kills.ToString());
-                scores[i].deaths.SetText(scoreData[i].Deaths.ToString());
-                scores[i].classText.SetText(scoreData[i].GameClass.ToString());
+                scores[i].nickName.SetText(scoreBoardData[i].NickName);
+                scores[i].kills.SetText(scoreBoardData[i].Kills.ToString());
+                scores[i].deaths.SetText(scoreBoardData[i].Deaths.ToString());
+                scores[i].classText.SetText(scoreBoardData[i].GameClass.ToString());
+                if (_avatarBySteamId.TryGetValue(scoreBoardData[i].SteamID, out var avatarTexture))
+                    scores[i].avatar.texture = avatarTexture;
+                else
+                {
+                    _avatarLoader.RequestAvatar(scoreBoardData[i].SteamID);
+                }
                 scores[i].gameObject.SetActive(true);
             }
+        }
+
+        private void ApplyAvatar(CSteamID steamID, Texture2D steamAvatar)
+        {
+            _avatarBySteamId[steamID] = steamAvatar;
         }
     }
 }
