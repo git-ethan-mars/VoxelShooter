6b04e9b673f9562c9a832e9a46b657de022c0df0
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2023-07-16 11:51:16 +0500

    Add pushable object script

---
+++ Assets/Scripts/Entities/PushableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Entities
+{
+    public class PushableObject : MonoBehaviour
+    {
+        public Vector3Int Center => Vector3Int.FloorToInt(transform.position);
+        public Vector3Int Min => new(-Size.x / 2, -Size.y / 2, -Size.z / 2);
+        public Vector3Int Max => new(Size.x / 2, Size.y / 2, Size.z / 2);
+        private Vector3Int Size { get; set; }
+        
+        private void Awake()
+        {
+            var bounds = GetComponent<Collider>().bounds;
+            Size = Vector3Int.FloorToInt(bounds.size);
+        }
+    }
+}
