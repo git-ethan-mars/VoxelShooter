b6f79a179aeebef0a152ef278e8c80ba6dde75e4
git-ethan-mars <73284049+git-ethan-mars@users.noreply.github.com>
2023-07-16 11:51:01 +0500

    Update Constants.cs

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,5 +1,9 @@
-﻿public static class Constants
+﻿using UnityEngine;
+
+public static class Constants
 {
+    public static LayerMask AttackMask = LayerMask.GetMask("Player") | LayerMask.GetMask("Chunk");
+    public static LayerMask BuildMask = LayerMask.GetMask("Chunk");
     public const float Epsilon = 0.001f;
     public const int DefaultFov = 60;
 }
