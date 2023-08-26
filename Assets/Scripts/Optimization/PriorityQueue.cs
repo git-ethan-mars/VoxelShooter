c8029d61ae5ba61a3e3a0d5e699d999f977a7976
a.burgart02 <a.burgart02@gmail.com>
2023-08-27 02:50:53 +0500

    Add nullable annotation

--- Assets/Scripts/Optimization/PriorityQueue.cs
+++ Assets/Scripts/Optimization/PriorityQueue.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+#nullable enable
 
 namespace Optimization
 {
