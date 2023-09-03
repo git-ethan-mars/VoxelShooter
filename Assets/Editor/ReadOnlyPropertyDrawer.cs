57f8f50239d3d37cbc261ba312c860bb6a6a1907
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2023-09-04 02:21:17 +0500

    Add ReadOnlyAttrbute

---
+++ Assets/Editor/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,16 @@
+﻿using CustomAttributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label);
+        }
+    }
+}
