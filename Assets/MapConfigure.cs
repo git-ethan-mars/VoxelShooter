bdcc715322fd2e3096ba0cb9ccc692f9b0737e5b
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2023-09-04 02:23:31 +0500

    Add MapConfigure SO

---
+++ Assets/MapConfigure.cs
@@ -0,0 +1,28 @@
+﻿using CustomAttributes;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MapConfigure : ScriptableObject
+{
+    [ReadOnly]
+    public string mapName;
+
+    [Header("Lighting")]
+    [ReadOnly]
+    public Vector3 lightPosition;
+
+    [ReadOnly]
+    public Quaternion lightRotation;
+
+    [ReadOnly]
+    public Color lightColor;
+
+    [Header("Skybox")]
+    [ReadOnly]
+    public Material skyboxMaterial;
+
+    private void OnValidate()
+    {
+        mapName = name;
+    }
+}
