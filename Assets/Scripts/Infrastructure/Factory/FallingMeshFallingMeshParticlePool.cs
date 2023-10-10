320be030e3ea9fc907b09f43732c5787adbaf7ef
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2023-10-10 22:59:39 +0500

    Refactor code and parent for falling mesh particles

--- Assets/Scripts/Infrastructure/Factory/FallingMeshFallingMeshParticlePool.cs
+++ Assets/Scripts/Infrastructure/Factory/FallingMeshFallingMeshParticlePool.cs
@@ -1,39 +1,21 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Pool;
-using Object = UnityEngine.Object;
 
 namespace Infrastructure.Factory
 {
     public class FallingMeshFallingMeshParticlePool : IFallingMeshParticlePool
     {
-        private Stack<ParticleSystem> _stack = new Stack<ParticleSystem>();
-        private const int ObjectCount = 10000;
-        
-        public FallingMeshFallingMeshParticlePool(IParticleFactory particleFactory)
+        private const int PoolSize = 1000;
+        private const string ContainerName = "FallingMeshParticleContainer";
+        private readonly Stack<ParticleSystem> _stack = new(PoolSize);
+
+        public FallingMeshFallingMeshParticlePool(IGameFactory gameFactory, IParticleFactory particleFactory)
         {
-            // _objectPool = new ObjectPool<ParticleSystem>(createFunc: particleFactory.CreateFallingMeshParticle,
-            //     actionOnGet: (particleSystem) => particleSystem.gameObject.SetActive(true), 
-            //     actionOnRelease: (obj) => obj.gameObject.SetActive(false), 
-            //     actionOnDestroy: (obj) => Object.Destroy(obj.gameObject), 
-            //     collectionCheck: false, defaultCapacity: ObjectCount, maxSize: ObjectCount);
-
-            // var particleSystems = new List<ParticleSystem>();
-            // for (var i = 0; i < ObjectCount; i++)
-            // {
-            //     particleSystems.Add(_objectPool.Get());
-            // }
-            //
-            // foreach (var VARIABLE in particleSystems)
-            // {
-            //     _objectPool.Release(VARIABLE);
-            // }
-
-            for (var i = 0; i < ObjectCount; i++)
+            var particleContainer = gameFactory.CreateGameObjectContainer(ContainerName).transform;
+            for (var i = 0; i < PoolSize; i++)
             {
-                var obj = particleFactory.CreateFallingMeshParticle();
+                var obj = particleFactory.CreateFallingMeshParticle(particleContainer);
                 obj.gameObject.SetActive(false);
                 _stack.Push(obj);
             }
@@ -51,7 +33,7 @@ namespace Infrastructure.Factory
             particleSystem.gameObject.SetActive(false);
             _stack.Push(particleSystem);
         }
-        
+
         public IEnumerator ReleaseOnDelay(ParticleSystem particleSystem, float lifetime)
         {
             yield return new WaitForSeconds(lifetime);
