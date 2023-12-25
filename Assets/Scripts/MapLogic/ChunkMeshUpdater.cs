4b29673db3e542c5dbad606aeb2ea5c3ecb88284
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2023-12-25 16:28:05 +0500

    Rename ChunkMeshProvider to ChunkMeshUpdater

---
+++ Assets/Scripts/MapLogic/ChunkMeshUpdater.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using Data;
+using Generators;
+using Networking;
+
+namespace MapLogic
+{
+    public class ChunkMeshUpdater
+    {
+        private readonly ChunkMesh[] _chunkMeshes;
+        private readonly IClient _client;
+
+        public ChunkMeshUpdater(IClient client, ChunkMesh[] chunkMeshes)
+        {
+            _chunkMeshes = chunkMeshes;
+            _client = client;
+            client.MapUpdated += OnMapUpdated;
+        }
+
+        public void Dispose()
+        {
+            _client.MapUpdated -= OnMapUpdated;
+        }
+
+        
+        private void OnMapUpdated(BlockDataWithPosition[] updatedBlocks)
+        {
+            var blocksByChunkIndex = updatedBlocks
+                .GroupBy(data => _client.MapProvider.GetChunkNumberByGlobalPosition(data.Position),
+                    data =>
+                        new BlockDataWithPosition(_client.MapProvider.GetLocalPositionByGlobal(data.Position),
+                            data.BlockData))
+                .ToDictionary(group => group.Key, group => group.ToList());
+
+            foreach (var (chunkIndex, blocks) in blocksByChunkIndex)
+            {
+                _chunkMeshes[chunkIndex].SpawnBlocks(blocks);
+            }
+        }
+    }
+}
