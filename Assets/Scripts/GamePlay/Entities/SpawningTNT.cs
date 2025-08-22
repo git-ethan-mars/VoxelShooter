eac1ec684ec626ae403c575024b7f090de30f7d5
git-ethan-mars <dima.nesterov.2002@inbox.ru>
2025-08-22 17:56:08 +0500

    Add FastScriptReload asset

---
+++ Assets/Scripts/GamePlay/Entities/SpawningTNT.cs
@@ -0,0 +1,24 @@
+﻿using Cysharp.Threading.Tasks;
+using Data;
+using Mirror;
+using Reflex.Attributes;
+using UnityEngine;
+namespace GamePlay
+{
+	public class SpawningTNT : NetworkBehaviour
+	{
+		private TNTData _data;
+
+		[Inject]
+		private void Construct(IInventoryItemDataLoader dataLoader)
+		{
+			_data = dataLoader.LoadItemData<TNTData>(ItemType.TNT);
+		}
+
+		public async UniTask ExplodeAsync()
+		{
+			await UniTask.WaitForSeconds(_data.DelayInSeconds, cancellationToken:destroyCancellationToken);
+			Debug.Log("TNT EXPLODED");
+		}
+	}
+}
